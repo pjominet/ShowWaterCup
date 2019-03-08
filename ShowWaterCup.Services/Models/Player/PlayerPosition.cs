25ce6824daacb3b6554831502bb07e802918a46d
blyatrick <jompa010@gmail.com>
2019-03-08 21:22:04 +0100

    added move check

--- ShowWaterCup.Services/Models/Player/PlayerPosition.cs
+++ ShowWaterCup.Services/Models/Player/PlayerPosition.cs
@@ -1,8 +1,27 @@
+using ShowWaterCup.Services.Models.Enums;
+
 namespace ShowWaterCup.Services.Models.Player
 {
     public class PlayerPosition
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public bool CanMove(Directions directions)
+        {
+            switch (directions)
+            {
+                case Directions.Up:
+                    return Y > 0;
+                case Directions.Down:
+                    return Y < 9;
+                case Directions.Left:
+                    return X > 0;
+                case Directions.Right:
+                    return X < 9;
+                default:
+                    return false;
+            }
+        }
     }
 }
