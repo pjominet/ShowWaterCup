75329f7e635b5efc0973bbcf08d259251143c4d6
Elisa Rass <elisa@linc.lu>
2019-03-08 21:55:20 +0100

    Add services for PlayerAI and playerAIStep

--- ShowWaterCup.Services/Contracts/IPlayerAIService.cs
+++ ShowWaterCup.Services/Contracts/IPlayerAIService.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ShowWaterCup.Services.Models;
+using ShowWaterCup.Services.Entities;
 
 namespace ShowWaterCup.Services.Contracts
 {
     public interface IPlayerAIService
     {
-        
+        PlayerAI GetPlayerAI(int AiId);
+        int CreatePlayerAI(PlayerAI playerAI);
+        void UpdatePlayerAI(PlayerAI playerAI);
     }
 }
--- ShowWaterCup.Services/Services/PlayerAIService.cs
+++ ShowWaterCup.Services/Services/PlayerAIService.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShowWaterCup.Services.Contracts;
+using ShowWaterCup.Services.Entities;
+using ShowWaterCup.Services.Repositories;
 
 namespace ShowWaterCup.Services.Services
 {
-    public class PlayerAIService
+    public class PlayerAIService : IPlayerAIService
     {
+        private readonly PlayerAIRepository _playerAIRepository;
+
+        public PlayerAIService()
+        {
+            var context = new Entities.ShowWaterCupEntities();
+            _playerAIRepository = new PlayerAIRepository(context);
+        }
+
+        public int CreatePlayerAI(PlayerAI playerAI)
+        {
+            _playerAIRepository.Add(playerAI);
+            _playerAIRepository.SaveChanges();
+            return playerAI.AiId;
+        }
+
+        public PlayerAI GetPlayerAI(int AiId)
+        {
+            return _playerAIRepository.GetFirst<PlayerAI>(ai => ai.AiId == AiId);
+        }
+
+        public void UpdatePlayerAI(PlayerAI playerAI)
+        {
+            var entity = _playerAIRepository.GetFirst<PlayerAI>(ai => ai.AiId == playerAI.AiId);
+            entity = playerAI;
+            _playerAIRepository.SaveChanges();
+        }
     }
 }
