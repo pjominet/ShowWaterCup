e04ea1242077795f8af8a5a6edc1e89ecc1c9e9a
Lionel <mottet.lionel@gmail.com>
2019-03-09 02:54:15 +0100

    xmlSerialize and value objects

---
+++ ShowWaterCup.Services/Models/Blockly/Field.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace ShowWaterCup.Services.Models.Blockly
+{
+    [XmlRoot(ElementName = "field", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Field
+    {
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+}
---
+++ ShowWaterCup.Services/Models/Blockly/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace ShowWaterCup.Services.Models.Blockly
+{
+    [XmlRoot(ElementName = "value", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Value
+    {
+        [XmlElement(ElementName = "block", Namespace = "http://www.w3.org/1999/xhtml")]
+        public Block Block { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+    }
+}
